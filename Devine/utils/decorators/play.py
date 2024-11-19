5f0eade68999b2b739101126995d3f1cfac0d162
Lord-Devine <devineparadoxz@outlook.com>
2024-11-19 22:24:43 +0530

    Update play.py

--- Devine/utils/decorators/play.py
+++ Devine/utils/decorators/play.py
@@ -23,7 +23,6 @@ from Devine.utils.database import (
 from Devine.utils.inline import botplaylist_markup
 from configuration import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
-from configuration import OWNER_ID
 links = {}
 
 
@@ -194,28 +193,3 @@ def PlayWrapper(command):
 
     return wrapper
 
-
-from pyrogram import filters
-
-@app.on_message(filters.command("link") & filters.user(OWNER_ID))
-async def link_handler(client, message):
-    if len(message.command) < 2:
-        return await message.reply_text("ᴘʀᴏᴠɪᴅᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ᴛᴏ ʀᴇᴛʀɪᴇᴠᴇ ɪᴛs ʟɪɴᴋ.")
-
-    target_chat_id = message.command[1]
-
-    try:
-        chat = await app.get_chat(int(target_chat_id))
-        if chat.username:
-            chat_link = f"https://t.me/{chat.username}"
-        else:
-            chat_link = await app.export_chat_invite_link(int(target_chat_id))
-
-        await message.reply_text(f"ʜᴇʀᴇ ɪs ᴛʜᴇ ᴄʜᴀᴛ ʟɪɴᴋ ғᴏʀ {chat.title}: {chat_link}")
-
-    except ChatAdminRequired:
-        return await message.reply_text("ɪ'ᴍ ɴᴏᴛ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴛᴀʀɢᴇᴛ ᴄʜᴀᴛ ᴏʀ ɴᴏᴛ ᴛʜᴇʀᴇ.")
-    except Exception as e:
-        return await message.reply_text(
-            f"An error occurred: {type(e).__name__}. Please try again later."
-        )
