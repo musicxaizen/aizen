34bc921d0fdf8cd19a1520afea9add8ffd0bf120
Soulaizen77 <hritikraj123412312@gmail.com>
2024-12-07 17:15:17 +0530

    Update start.py

--- Devine/programs/bot/start.py
+++ Devine/programs/bot/start.py
@@ -43,7 +43,7 @@ async def start_pm(client, message: Message, _):
     await asyncio.sleep(0.1)
     await loading_1.delete()
 
-    started_msg = await message.reply_text(text="<b>sᴛᴀʀᴛᴇᴅ...<a href='https://envs.sh/5ci.mp4' target='_blank'>ㅤ ㅤㅤㅤ</a></b>")
+    started_msg = await message.reply_text(text="<b>sᴛᴀʀᴛᴇᴅ...<a href='https://envs.sh/KZQ.jpg' target='_blank'>ㅤ ㅤㅤㅤ</a></b>")
     await asyncio.sleep(0.4)
     await started_msg.delete()
 
