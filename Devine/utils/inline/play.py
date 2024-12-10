7a26cfd109cbcff53a72d105dff58e19b8102f6f
Soulaizen77 <hritikraj123412312@gmail.com>
2024-12-10 16:29:16 +0530

    Update play.py

--- Devine/utils/inline/play.py
+++ Devine/utils/inline/play.py
@@ -77,9 +77,6 @@ def stream_markup_timer(_, chat_id, played, dur):
             [
                 InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
             ],
-            [   
-                InlineKeyboardButton(text="Oᴜʀ sᴜᴘᴘᴏʀᴛ",url=f"https://t.me/Friend_Chat_International"),
-            ],
         ]
         return buttons
     except Exception as e:
@@ -100,9 +97,6 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
         ],
-        [
-            InlineKeyboardButton(text="Oᴜʀ sᴜᴘᴘᴏʀᴛ",url=f"https://t.me/Friend_Chat_International"),
-        ]
     ]
     return buttons
 
